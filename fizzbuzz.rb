9a4dc06498a3a8d9016d8490c03037cdba90c861
mbranting <mckenna.branting@gmail.com>
2018-06-05 19:13:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz
-  int = get.strip.to_i
-if int % 3 && int !% 5
+  int = get.chomp.to_i
+if "#{int}" % 3 && "#{int}" !% 5
     puts "Fizz"
-elsif int % 5 && int !% 3
+elsif "#{int}" % 5 && int !% 3
     puts "Buzz"
-elsif int % 3 && int % 5 
+elsif "#{int}" % 3 && int % 5 
     puts "Fizzbuzz"
 else 
     puts "nil"
