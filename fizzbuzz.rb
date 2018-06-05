005cb69d88be4224a11c531ea75d71746b8ec945
mbranting <mckenna.branting@gmail.com>
2018-06-05 19:10:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,10 @@
 def fizzbuzz
-  number = get.strip.to_i
-  if "#{number}" % 3 = 
+  int = get.strip.to_i
+  if int % 3 && int !% 5
+    puts "Fizz"
+  elsif int % 5 && int !% 3
+    puts "Buzz"
+  elsif int % 3 && int % 5 
+    puts "Fizzbuzz"
+  else 
+    puts "nil"
